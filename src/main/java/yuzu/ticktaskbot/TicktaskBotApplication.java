23b304ff4d34d19bcb5abb75ba1bca71dbc628b7
YuZu <yurkazubkov@yandex.ru>
2023-11-25 16:22:09 +0300

    init

---
+++ src/main/java/yuzu/ticktaskbot/TicktaskBotApplication.java
@@ -0,0 +1,13 @@
+package yuzu.ticktaskbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicktaskBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicktaskBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yuzu/ticktaskbot/TicktaskBotApplicationTests.java
@@ -0,0 +1,13 @@
+package yuzu.ticktaskbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicktaskBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
